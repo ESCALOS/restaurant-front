b6c519594ee161d6fc1ab2b2405210fb4904a853
Nanoka <stornblood6969@gmail.com>
2024-11-02 00:32:02 -0500

    added debounce to filter user

--- src/hooks/useUserFilter.ts
+++ src/hooks/useUserFilter.ts
@@ -7,13 +7,19 @@ export const useUserFilter = (users: User[]) => {
   const [filteredUsers, setFilteredUsers] = useState<User[]>(users);
 
   useEffect(() => {
-    const results = users.filter((user) => {
-      const matchesSearch = user.name
-        .toLowerCase()
-        .includes(searchQuery.toLowerCase());
-      return matchesSearch && user.is_enabled === showEnabled;
-    });
-    setFilteredUsers(results);
+    // Almacenamos el timeout para poder limpiarlo si el usuario sigue escribiendo
+    const delayDebounce = setTimeout(() => {
+      const results = users.filter((user) => {
+        const matchesSearch =
+          user.name.toLowerCase().includes(searchQuery.toLowerCase()) ||
+          user.document_number.includes(searchQuery);
+        return matchesSearch && user.is_enabled === showEnabled;
+      });
+      setFilteredUsers(results);
+    }, 250); // Retraso de 300 ms
+
+    // Limpiar el timeout si el usuario sigue escribiendo o si hay cambios en los otros filtros
+    return () => clearTimeout(delayDebounce);
   }, [searchQuery, showEnabled, users]);
 
   return {
